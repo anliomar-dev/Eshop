d2059f12a1d669bfe42389e11bbb93e65b44390a
Anli omar <omaranli56@gmail.com>
2024-10-10 09:19:25 +0400

    initial commit

--- api/models.py
+++ api/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=14, unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    addresse = models.CharField(max_length=100, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Product:
+    pass
+
+
+class Order:
+    pass
+
+
+class OrderItem:
+    pass
+
+
+class Cart:
+    pass
+
+
+class ShippingAdress:
+    pass
+
+
+class Payment:
+    pass
+
+
+class Review:
+    pass
+
+
+class Coupon:
+    pass
+
+
+class Promo:
+    pass
+
+
+class Category:
+    pass
+
+
+class Color:
+    pass
+
+
+class Image:
+    pass
+
+
+class Brand:
+    pass
+
+
+class Variant:
+    pass
+
--- commerce/settings.py
+++ commerce/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'Eshop',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
